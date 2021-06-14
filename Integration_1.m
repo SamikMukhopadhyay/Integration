225fe4f799468eed96ee6335b279a801c05538b5
Samik Mukhopadhyay <53707970+SamikMukhopadhyay@users.noreply.github.com>
2021-06-14 23:50:17 +0530

    Initialization

---
+++ Integration_1.m
@@ -0,0 +1,17 @@
+clc; clear;
+
+number_of_steps = 10000;
+f = @(x) sin(x);
+ 
+lowlim = 0;
+highlim = pi;
+
+steps = (highlim - lowlim)/number_of_steps;
+temp = 0;
+
+for i = 1:number_of_steps
+  x = lowlim + (i-0.5)*steps;
+  temp += f(x);
+endfor
+
+answer  = temp*(highlim-lowlim)/number_of_steps
